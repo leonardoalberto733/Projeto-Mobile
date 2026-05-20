bb55c0030782a68d5dd901bbe03b9ce5d1dc8d0d
Leticia Sanches Silva <leticiasancheslion@gmail.com>
2026-05-20 07:57:45 -0300

    fix: corrige a URL e a KEY do supabase

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -2,8 +2,8 @@ import 'react-native-url-polyfill/auto';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://wrsnielsnrrjlconcxqu.supabase.co/rest/v1/';
-const supabaseKey = 'sb_publishable_uaH_RZ_cCMP9hMYclkxXNA_wc3wfY7g';
+const supabaseUrl = 'https://wrsnielsnrrjlconcxqu.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indyc25pZWxzbnJyamxjb25jeHF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2MzQ4NTAsImV4cCI6MjA5NDIxMDg1MH0.cXsqv6XuHxxaKth3qIBvM4gBwqXYXT85YbcPMwHZcjs';
 
 export const supabase = createClient(supabaseUrl, supabaseKey, {
   auth: {
