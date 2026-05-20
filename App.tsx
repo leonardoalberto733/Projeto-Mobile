d6cc7b0afbb832e42d9e1d6b853496e148b90254
Leticia Sanches Silva <leticiasancheslion@gmail.com>
2026-05-20 09:07:51 -0300

    fix: alterações dos arquivos de Tela para a pasta correta e correção dos imports

--- App.tsx
+++ App.tsx
@@ -1,9 +1,9 @@
 import { SafeAreaView, StyleSheet } from 'react-native';
-import TelaCadastro from './src/components/TelaCadastro';
-import TelaLogin from './src/components/TelaLogin';
-import TelaGrupos from './src/components/TelaGrupos';
-import TelaDetalhesGrupo from './src/components/TelaDetalhesGrupo';
-import TelaSaldo from './src/components/TelaSaldo';
+import TelaCadastro from './src/screens/TelaCadastro';
+import TelaLogin from './src/screens/TelaLogin';
+import TelaGrupos from './src/screens/TelaGrupos';
+import TelaDetalhesGrupo from './src/screens/TelaDetalhesGrupo';
+import TelaSaldo from './src/screens/TelaSaldo';
 
 export default function App() {
   return (
